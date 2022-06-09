7fdcba7df99136340484a47219a9c886f7af0f57
Christopher Powell <chrispowell1129@gmail.com>
2022-06-09 15:20:45 -0400

    added @EnablePrometheusEndpoint annotation

--- src/main/java/com/revature/project1/Project1Application.java
+++ src/main/java/com/revature/project1/Project1Application.java
@@ -1,9 +1,11 @@
 package com.revature.project1;
 
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnablePrometheusEndpoint
 public class Project1Application {
 
 	public static void main(String[] args) {
