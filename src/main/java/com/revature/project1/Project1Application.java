a911ddc036bea167b180b0c959f8cbc8bda469f8
Christopher Powell <chrispowell1129@gmail.com>
2022-06-13 10:05:42 -0400

    added timedAspect to application class

--- src/main/java/com/revature/project1/Project1Application.java
+++ src/main/java/com/revature/project1/Project1Application.java
@@ -1,15 +1,21 @@
 package com.revature.project1;
 
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-//@EnablePrometheusEndpoint
 public class Project1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Project1Application.class, args);
 	}
 
+	@Bean
+	public TimedAspect timedAspect(MeterRegistry registry){
+		return new TimedAspect(registry);
+	}
 }
